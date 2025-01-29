4389d0850b0d2ddd30163dde755a6edf743c3afe
henrique <henriquebaslaet@hotmail.com>
2025-01-29 15:39:17 -0300

    Creating controller

---
+++ src/main/java/com/Laet/email_services/controllers/EmailSenderController.java
@@ -0,0 +1,34 @@
+package com.Laet.email_services.controllers;
+
+import com.Laet.email_services.application.EmailSenderService;
+import com.Laet.email_services.core.exception.EmailServiceException;
+import com.amazonaws.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/email")
+public class EmailSenderController {
+
+    private final EmailSenderService emailSenderService;
+
+    @Autowired
+    public EmailSenderController(EmailSenderService emailSenderService) {
+        this.emailSenderService = emailSenderService;
+    }
+
+    @PostMapping()
+    public ResponseEntity<String> sendEmail(@RequestBody EmailRequest request) {
+        try {
+            this.emailSenderService.sendEmail(request.to(), request.subject(), request.body());
+            return ResponseEntity.ok("email send successfully");
+        } catch(EmailServiceException ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error while sending email");
+        }
+    }
+}
